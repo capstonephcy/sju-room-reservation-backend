86143d6b44f0e96935523d245750508e9a5cd4ce
newcentury99 <smyun99@gmail.com>
2023-03-17 15:20:30 +0900

    Scaffold Projects

---
+++ src/main/java/com/sju/roomreservationbackend/RoomReservationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sju.roomreservationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomReservationBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomReservationBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sju/roomreservationbackend/RoomReservationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sju.roomreservationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomReservationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
