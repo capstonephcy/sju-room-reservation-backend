f5bfb9b319c9d7ae16fa3085458f95c8318bc6fd
Seongmin Yun <smyun99@gmail.com>
2023-05-31 03:19:31 +0900

    Add checkInCode Send to device logic

--- src/main/java/com/sju/roomreservationbackend/domain/room/profile/dto/response/FetchRoomForDeviceResDTO.java
+++ src/main/java/com/sju/roomreservationbackend/domain/room/profile/dto/response/FetchRoomForDeviceResDTO.java
@@ -13,4 +13,5 @@ import java.util.List;
 public class FetchRoomForDeviceResDTO extends GeneralPageableResDTO {
     private Room room;
     private Reservation currentRev;
+    private String code;
 }
